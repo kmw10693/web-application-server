2abc9e17318994b757d647a2c77908097db7b179
kmw10693 <kmw10693@konkuk.ac.kr>
2024-02-25 22:53:33 +0900

    initial commit

---
+++ src/main/java/webserver/RequestHandler.java
@@ -0,0 +1,88 @@
+package webserver;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RequestHandler extends Thread {
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String LinuxDir = "./webapp";
+
+    private Socket connection;
+
+    public RequestHandler(Socket connectionSocket) {
+        this.connection = connectionSocket;
+    }
+
+    public void run() {
+        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+                connection.getPort());
+
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            List<String> strings = getLines(in);
+
+            String line = strings.get(0);
+            String[] tokens = line.split(" ");
+
+            byte[] body = getFilebody(tokens);
+
+            DataOutputStream dos = new DataOutputStream(out);
+            response200Header(dos, body.length);
+            responseBody(dos, body);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private byte[] getFilebody(String[] tokens) throws IOException {
+        byte[] body;
+        body = Files.readAllBytes(Paths.get(LinuxDir + tokens[1]));
+        return body;
+    }
+
+    private List<String> getLines(InputStream in) {
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+
+        List<String> strings = new ArrayList<>();
+        try {
+            String str;
+
+            while(!(str = br.readLine()).equals("")) {
+                strings.add(str);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return strings;
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void responseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
