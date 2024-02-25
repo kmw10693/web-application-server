efb348f4f100014bb39f87e0f5e3dfd8946d573c
kmw10693 <kmw10693@konkuk.ac.kr>
2024-02-25 23:46:08 +0900

    [feat] 요구사항 2 - GET 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -8,9 +8,12 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -34,6 +37,13 @@ public class RequestHandler extends Thread {
             String line = strings.get(0);
             String[] tokens = line.split(" ");
 
+            int index = tokens[1].indexOf("?");
+
+            if(index != -1) {
+                String params = getParams(tokens);
+                createUser(params);
+            }
+
             byte[] body = getFilebody(tokens);
 
             DataOutputStream dos = new DataOutputStream(out);
@@ -44,9 +54,25 @@ public class RequestHandler extends Thread {
         }
     }
 
+    private void createUser(String params) {
+        Map<String, String> stringStringMap = HttpRequestUtils.parseQueryString(params);
+        User user = new User(
+                stringStringMap.get("userId"),
+                stringStringMap.get("password"),
+                stringStringMap.get("name"),
+                stringStringMap.get("email")
+        );
+    }
+
+    private String getParams(String[] tokens) {
+        int index = tokens[1].indexOf("?");
+        String requestPath = tokens[1].substring(0, index);
+        return tokens[1].substring(index+1);
+    }
+
     private byte[] getFilebody(String[] tokens) throws IOException {
         byte[] body;
-        body = Files.readAllBytes(Paths.get(LinuxDir + tokens[1]));
+        body = Files.readAllBytes(new File(LinuxDir + tokens[1]).toPath());
         return body;
     }
 
