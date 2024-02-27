e573fe924f2fd12ecf60e48698ad03032e5fc58c
kmw10693 <kmw10693@konkuk.ac.kr>
2024-02-27 23:46:16 +0900

    [feat] 요구사항 5 - 로그인하기 적용

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,11 +10,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import util.IOUtils;
+
+import javax.xml.crypto.Data;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -33,24 +36,47 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-
             InputStreamReader reader = new InputStreamReader(in);
             BufferedReader br = new BufferedReader(reader);
 
             List<String> strings = getLines(br);
+            System.out.println(strings);
             String line = strings.get(0);
             String[] tokens = line.split(" ");
 
-            // POST 방식의 회원가입
-            if(tokens[0].contains("POST")) {
+            // POST 방식의 회원가입`
+            if(tokens[1].equals("/user/create")) {
                 String[] split = strings.get(3).split(": ");
                 String s = IOUtils.readData(br, Integer.parseInt(split[1]));
+                createUser(s);
 
                 DataOutputStream dos = new DataOutputStream(out);
                 response302Header(dos);
 
             }
 
+            if(tokens[1].equals("/user/login")) {
+                String[] split = strings.get(3).split(": ");
+                String s = IOUtils.readData(br, Integer.parseInt(split[1]));
+
+                Map<String, String> map = HttpRequestUtils.parseQueryString(s);
+
+                try {
+                    User user = DataBase.findUserById(map.get("userId"));
+                    if(user.getPassword().equals(map.get("password"))) {
+                        DataOutputStream dos = new DataOutputStream(out);
+                        responseLoginHeader(dos);
+                    }
+                    else {
+                        DataOutputStream dos = new DataOutputStream(out);
+                        responseLoginFailHeader(dos);
+                    }
+
+                } catch (NullPointerException e) {
+                    log.error(e.getMessage());
+                }
+
+            }
             // GET 방식의 회원가입
             int index = tokens[1].indexOf("?");
 
@@ -71,14 +97,16 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private User createUser(String params) {
+    private void createUser(String params) {
         Map<String, String> stringStringMap = HttpRequestUtils.parseQueryString(params);
-        return new User(
+
+        User user = new User(
                 stringStringMap.get("userId"),
                 stringStringMap.get("password"),
                 stringStringMap.get("name"),
                 stringStringMap.get("email")
         );
+        DataBase.addUser(user);
     }
 
     private String getParams(String[] tokens) {
@@ -119,6 +147,28 @@ public class RequestHandler extends Thread {
         }
     }
 
+    private void responseLoginHeader(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html\r\n");
+            dos.writeBytes("Set-Cookie: logined=true");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void responseLoginFailHeader(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html\r\n");
+            dos.writeBytes("Set-Cookie: logined=false");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
     private void response302Header(DataOutputStream dos) {
         try {
             dos.writeBytes("HTTP/1.1 302 Found \r\n");
@@ -128,6 +178,17 @@ public class RequestHandler extends Thread {
             log.error(e.getMessage());
         }
     }
+
+    private void response302FailHeader(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: http://localhost:8080/login_failed.html\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
 
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
